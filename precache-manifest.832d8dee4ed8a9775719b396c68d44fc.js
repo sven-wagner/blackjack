b1e17ebf260e55f36a73a2720c55448daefb6979
sven-wagner <61547361+sven-wagner@users.noreply.github.com>
2020-06-22 22:15:58 +0200

    Updates

---
+++ precache-manifest.832d8dee4ed8a9775719b396c68d44fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a9f207b33872d2445e17a58344a621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1047afe91ff8a0b2ab54",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e4b769f0b2f190934ae1",
+    "url": "/static/css/main.d8cbc6a7.chunk.css"
+  },
+  {
+    "revision": "1047afe91ff8a0b2ab54",
+    "url": "/static/js/2.81097fc7.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/static/js/2.81097fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4b769f0b2f190934ae1",
+    "url": "/static/js/main.027df91c.chunk.js"
+  },
+  {
+    "revision": "57005b988df560609e80",
+    "url": "/static/js/runtime-main.9817ed3b.js"
+  }
+]);
